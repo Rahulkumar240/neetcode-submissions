7515784591b4bf33dd276407cb6c2b966aa68729
Rahul Kumar <rahulsharma9709397@gmail.com>
2026-05-16 23:40:25 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> ans(nums.begin(),nums.end());
+
+        return ans.size() != nums.size();
+
+    }
+};
